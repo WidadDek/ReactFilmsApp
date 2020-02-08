166466a39d11bef46057ab3e454428f4d6888f53
Wididou <fw_dekkiche@esi.dz>
2020-02-08 15:50:19 +0100

    add infinit scroll and navigation API

--- Components/Search.js
+++ Components/Search.js
@@ -1,59 +1,84 @@
 // Components/Search.js
 
 import React from 'react'
-import films from '../Helpers/filmsData'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
-import { StyleSheet, View, TextInput, Button, Text, FlatList,ActivityIndicator } from 'react-native'
-import {getFilmsFromApiWithSearchedText} from '../API/TMDBApi'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
+    this.searchedText = ""
+    this.page = 0
+    this.totalPages = 0
+    this.state = {
       films: [],
-      isLoading:false }
-    this.searchedText=""
-  }
-
-  _loadFilms(){
-    this.setState({isLoading:true})
-    if(this.searchedText.length>0){
-      getFilmsFromApiWithSearchedText(this.searchedText).then((data) =>
-      this.setState({
-        films:data.results,
-        isLoading:false
-        }))
+      isLoading: false
     }
   }
 
+  _loadFilms() {
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true })
+      getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+          this.page = data.page
+          this.totalPages = data.total_pages
+          this.setState({
+            films: [ ...this.state.films, ...data.results ],
+            isLoading: false
+          })
+      })
+    }
+  }
+
+  _searchTextInputChanged(text) {
+    this.searchedText = text 
+  }
+
+  _searchFilms() {
+    this.page = 0
+    this.totalPages = 0
+    this.setState({
+      films: [],
+    }, () => {
+        this._loadFilms()
+    })
+  }
+
   _displayLoading() {
-    if(this.state.isLoading){
+    if (this.state.isLoading) {
       return (
         <View style={styles.loading_container}>
-          <ActivityIndicator size='large'/>
+          <ActivityIndicator size='large' />
         </View>
       )
     }
   }
 
-  _searchTextInputChanged(text){
-    this.searchedText=text
-  }
-
   render() {
     return (
       <View style={styles.main_container}>
-      <TextInput onSubmitEditing={() =>this._loadFilms()} onChangeText={(text) => this._searchTextInputChanged(text)} style={styles.textinput} placeholder='Titre du film'/>
-      <Button title='Rechercher' onPress={() =>
-      this._loadFilms()}/>
-      <FlatList
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._searchFilms()}
+        />
+        <Button title='Rechercher' onPress={() => this._searchFilms()}/>
+        <FlatList
           data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) =><FilmItem film={item}/>}
+          renderItem={({item}) => <FilmItem film={item}/>}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+              if (this.page < this.totalPages) {
+                 this._loadFilms()
+              }
+          }}
         />
         {this._displayLoading()}
-          </View>
+      </View>
     )
   }
 }
@@ -63,15 +88,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20
   },
-  loading_container:{
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top:0,
-    bottom:0,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
   textinput: {
     marginLeft: 5,
     marginRight: 5,
@@ -79,6 +95,15 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
