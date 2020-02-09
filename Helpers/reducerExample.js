8646d2fbbbfa40643ed7601d10460eade90fc969
Wididou <fw_dekkiche@esi.dz>
2020-02-09 01:09:44 +0100

    add redux & create first reducer

---
+++ Helpers/reducerExample.js
@@ -0,0 +1,19 @@
+function reducerProfil(state,action){
+    let nextState
+    switch(action.type){
+        case 'ADD-PROFIL':
+            nextState={
+                ...state,
+                profil: action.value
+            }
+            return nextState
+        case 'UPDATE-PROFIL':
+            return nextState
+        case 'DELETE-PROFIL':
+            return nextState
+        default:
+            return nextState
+    }
+
+    return nextState
+}
---
+++ Store/Reducers/favoriteReducer.js
@@ -0,0 +1,30 @@
+const initialState= {favoritesFilm: []}
+
+function toggleFavorite(state,action){
+    let nextState
+    switch (action.type) {  
+        case 'TOGGLE_FAVORITE':
+            const favoriteFilmIndex = state.favoritesFilm.findIndex(
+                item => item.id === action.value.id )  
+            if (favoriteFilmIndex !== -1){ //principe d'immuable
+                //suppression 
+                nextState = {
+                    ...state,
+                    favoritesFilm: state.favoritesFilm.filter((item, index) =>
+                        index !== favoriteFilmIndex)
+                }
+            }else {
+                //ajouter
+                nextState = {
+                    ...state,
+                    favoritesFilm: [...state.favoritesFilm, action.value]
+                }
+            }
+            return nextState || state
+        default:
+            return state
+    }
+     
+}
+
+export default toggleFavorite
